c9f807929fa95e510fe0e35c1ca2018c4dc441bd
Madhur Gupta <madhurrgupta@gmail.com>
2023-12-28 12:39:16 +0530

    configure server in backend

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import {Server as socketIO} from "socket.io";
+import dotenv from "dotenv";
+import http from "http";
+import app from "./app.js";
+
+dotenv.config({ path: "dev.env" });
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+const io = new socketIO(server);
+
+server.listen(port, () => {
+	console.log(`App listening on port ${port}! 🔥`);
+});
