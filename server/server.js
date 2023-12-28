1031bf06317e7a4c0d42dc99cdde7fc0c29fc2ed
Madhur Gupta <madhurrgupta@gmail.com>
2023-12-28 14:43:49 +0530

    changed port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import app from "./app.js";
 
 dotenv.config({ path: "dev.env" });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 const io = new socketIO(server);
