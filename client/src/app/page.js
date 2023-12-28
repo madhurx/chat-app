5645bf31c05f4c038129ecb9ce0b3f7266fb6909
Madhur Gupta <madhurrgupta@gmail.com>
2023-12-28 18:02:31 +0530

    feat: added socket.io-client

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,7 +1,13 @@
+import io from "socket.io-client";
+
+const ENDPOINT = "http://localhost:3000/";
+const socket = io(ENDPOINT, { transports: ["websocket"] });
+
 export default function Home() {
+	socket.on("connect", () => {});
 	return (
 		<div>
-			<h1>HEllo</h1>
+			<h1>HOMEPAGE</h1>
 		</div>
 	);
 }
