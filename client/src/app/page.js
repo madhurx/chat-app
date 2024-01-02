0f26796117d9418f37c687fc964b6ef3d4ce52e0
Madhur Gupta <madhurrgupta@gmail.com>
2024-01-02 12:06:55 +0530

    feat: integrated socket.io

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,16 @@
 import io from "socket.io-client";
 
 const ENDPOINT = "http://localhost:3000/";
-const socket = io(ENDPOINT, { transports: ["websocket"] });
+const socket = io.connect(ENDPOINT, { transports: ["polling", "websocket", "flashsocket"] });
 
 export default function Home() {
-	socket.on("connect", () => {});
+	socket.on("connect", () => {
+		console.log("Connected to the server");
+	});
+	socket.on("connect_error", (err) => {
+		console.log(`connect_error due to ${err.message}`);
+	});
+
 	return (
 		<div>
 			<h1>HOMEPAGE</h1>
--- server/server.js
+++ server/server.js
@@ -10,12 +10,12 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new socketIO(server);
 
+//this will work when circuit gets on
+io.on("connection", (socket) => {
+	console.log("New Connection, Socket ID:", socket.id);
+});
+
 server.listen(port, () => {
 	console.log(`App listening on port ${port}! 🔥`);
 	console.log(`Server is working on http://localhost:${port}`);
-});
-
-//this will work when circuit gets on
-io.on("connection", () => {
-	console.log("New Connection");
 });
