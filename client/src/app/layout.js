1e779d309d9ab7a32518ad00187a434335d2626a
Madhur Gupta <madhurrgupta@gmail.com>
2024-01-02 17:26:37 +0530

    feat: roboto font

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,14 +1,21 @@
 import "./globals.css";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-roboto",
+	weight: ["300"],
+});
 
 export const metadata = {
 	title: "Chat App",
-	description: "Application for chating",
+	description: "Application for chatting",
 };
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
+		<html className={`${roboto.variable}`}>
+			<body className="m-0">{children}</body>
 		</html>
 	);
 }
