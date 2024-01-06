02cdf7efe4f4b2657bee7cd620e4b7b47132089d
Madhur Gupta <madhurrgupta@gmail.com>
2024-01-06 10:37:38 +0530

    feat: notifying user activity

--- client/src/app/chat/page.js
+++ client/src/app/chat/page.js
@@ -4,26 +4,50 @@ import { useEffect } from "react";
 import io from "socket.io-client";
 import SendIcon from "@mui/icons-material/Send";
 
+const ENDPOINT = "http://localhost:5000/";
+
 const page = () => {
-	const ENDPOINT = "http://localhost:5000/";
-	const socket = io(ENDPOINT, {
-		transports: ["websocket", "polling", "flashsocket"],
-	});
+	const searchParams = useSearchParams();
+	const username = searchParams.get("username");
 
 	useEffect(() => {
-		socket.on("connect", (data) => {
-			console.log("Received data from server:", data);
+		const socket = io(ENDPOINT, {
+			transports: ["websocket", "polling", "flashsocket"],
+		});
+
+		//on for receiving data
+		socket.on("connect", () => {
+			alert("Connected");
 		});
 
 		socket.on("connect_error", (err) => {
 			console.log(`Connect error due to ${err.message}`);
 		});
 
-		return () => {};
-	}, [socket]);
+		console.log(socket);
 
-	const searchParams = useSearchParams();
-	const name = searchParams.get("username");
+		//emit for sending data
+		socket.emit("joined", {
+			username,
+		});
+
+		socket.on("welcome", (data) => {
+			alert(data.message);
+		});
+
+		socket.on("userJoined", (data) => {
+			alert(data.message);
+		});
+
+		socket.on("leave", (data) => {
+			alert(data.message);
+		});
+
+		return () => {
+			socket.on("disconnect");
+			socket.off();
+		};
+	}, []);
 
 	return (
 		<div className="container w-full bg-gray-900 max-w-screen-2xl h-screen text-white">
@@ -31,7 +55,7 @@ const page = () => {
 				<div className="flex flex-col border-white border col-span-full h-[50vh] mx-auto w-[50vw] items-center">
 					<div className="basis-2/12 overflow-hidden bg-cyan-400 text-white w-full"></div>
 					<div className="basis-8/12 overflow-y-scroll overflow-x-hidden w-full">
-						{name}
+						{username}
 					</div>
 					<div className="basis-2/12 overflow-hidden text-black w-full bg-gray-200">
 						<div className="w-full h-full px-3 flex">
--- server/server.js
+++ server/server.js
@@ -10,11 +10,34 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new socketIO(server, { cors: { origin: "*" } });
 
+const users = [{}];
+
 //this will work when circuit gets on
 io.on("connect", (socket) => {
 	console.log("New User, Socket ID:", socket.id);
+
+	socket.on("joined", ({ username }) => {
+		users[socket.id] = username;
+		console.log(username + " has joined.");
+
+		socket.broadcast.emit("userJoined", {
+			user: "Admin",
+			message: `${users[socket.id]} has joined`,
+		});
+
+		socket.emit("welcome", {
+			user: "Admin",
+			message: `Welcome to the chat`,
+		});
+	});
+
 	socket.on("disconnect", () => {
-		console.log("User disconnected");
+		console.log(`user disconnected`);
+		socket.broadcast.emit("leave", {
+			user: "Admin",
+			message: `${users[socket.id]} has left the chat`,
+		});
+		socket.removeAllListeners();
 	});
 });
 
